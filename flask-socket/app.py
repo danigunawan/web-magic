b8d435cdb9064ce2b59468d0bb5ac79e55717d04
rickbarraza <rickbarraza@outlook.com>
2019-05-03 13:31:42 -0700

    got roundtrip from tf client to opencv server working

--- flask-socket/app.py
+++ flask-socket/app.py
@@ -3,8 +3,14 @@ import eventlet
 import time
 import base64
 
+import cv2
+import numpy as np
+
 from flask import Flask, render_template, session, request, current_app
 from flask_socketio import SocketIO, emit, disconnect
+
+# from flask_cors import CORS
+# cors = CORS(app, resources={r"/static/*": {"origins": "*"}})
 
 eventlet.monkey_patch()
 
@@ -33,7 +39,86 @@ def app_connect():
 
 @socketio.on('point_submit', namespace='/eq')
 def point_submit(data):
-    socketio.emit("point_returned", { 'new_line': data }, namespace="/eq")
+    response = getFace(data["data"])
+    socketio.emit("point_returned", response, namespace="/eq")
+
+@app.after_request
+def add_header(r):
+    """
+    Add headers to both force latest IE rendering engine or Chrome Frame,
+    and also to cache the rendered page for 10 minutes.
+    """
+    r.headers["Cache-Control"] = "no-cache, no-store, must-revalidate"
+    r.headers["Pragma"] = "no-cache"
+    r.headers["Expires"] = "0"
+    r.headers['Cache-Control'] = 'public, max-age=0'
+    return r
+
+# ============================================================
+def getFace(data):
+
+    # im = cv2.imread("headPose.jpg")
+    # size = im.shape
+
+    size = (data["webcam_height"], data["webcam_width"], 3)
+
+    image_points = np.array([
+        (data["nose"]["_x"],data["nose"]["_y"]),                # Nose 
+        (data["chin"]["_x"],data["chin"]["_y"]),                # chin
+        (data["left_eye"]["_x"],data["left_eye"]["_y"]),        # left eye left
+        (data["right_eye"]["_x"],data["right_eye"]["_y"]),      # rigth eye right
+        (data["left_mouth"]["_x"],data["left_mouth"]["_y"]),    # left mouth corner
+        (data["right_mouth"]["_x"],data["right_mouth"]["_y"])   # right mouth corner
+        ], dtype="double")
+
+    model_points = np.array([
+        (0.0, 0.0, 0.0),
+        (0.0, -330.0, -65.0),
+        (-225.0, 170.0, -135.0),
+        (225.0, 170.0, -135.0),
+        (-150.0, -150.0, -125.0),
+        (150.0, -150.0, -125.0),
+        ])
+
+    # Camera internals
+    focal_length = size[1]
+    # print("focal_length: ", focal_length)
+
+    center = ( size[1]/2, size[0]/2)
+    # print("center: ", center)
+
+    camera_matrix = np.array(
+        [ [focal_length, 0, center[0]],
+        [0, focal_length, center[1]],
+        [0,  0,  1]], dtype="double"
+    )
+    # print("Camera Matrix: ", camera_matrix)
+
+    dist_coeffs = np.zeros((4,1)) 
+    (success, rotation_vector, translation_vector) = cv2.solvePnP(model_points, image_points, camera_matrix, dist_coeffs, flags=cv2.SOLVEPNP_ITERATIVE)
+
+    print("Rotation Vector: ", rotation_vector )
+    print("Translation Vector: ", translation_vector)
+
+    # project a 3D point (0,0,1000) onto the image plane.
+    # We use this to draw a line sticking out of the nose.
+    (nose_end_point2D, jacobian) = cv2.projectPoints(np.array([ (0.0, 0.0, 500.0) ]), rotation_vector, translation_vector, camera_matrix, dist_coeffs)
+
+    bundle = {
+    "rot1" : rotation_vector[0][0],
+    "rot2" : rotation_vector[1][0],
+    'rot3' : rotation_vector[2][0],
+    'trans1' : translation_vector[0][0],
+    'trans2' : translation_vector[1][0],
+    'trans3' : translation_vector[2][0],
+    'forward_x' : nose_end_point2D[0][0][0],
+    'forward_y' : nose_end_point2D[0][0][1]
+    }
+
+    bundleformat = json.dumps(bundle)
+
+    return bundleformat
+    
 
 if __name__ == '__main__':
     socketio.run( app, port=8888, debug=True)
--- flask-socket/static/webcam.js
+++ flask-socket/static/webcam.js
@@ -23,6 +23,8 @@ async function setup() {
         canvas.height = webcam.height;
         sketch = canvas.getContext('2d')
         sketch.font = "11px Sans-serif";
+        console.log("webcam.width: " + webcam.width)
+        console.log("webcam.height: " + webcam.height)
     } 
 
     setupWebcam();
@@ -31,7 +33,6 @@ async function setup() {
         addOutput("loading model(s)...")
         await faceapi.nets.tinyFaceDetector.load('static/models/')
         await faceapi.loadFaceLandmarkModel('static/models/')
-        await faceapi.loadFaceRecognitionModel('static/models/')
         options = new faceapi.TinyFaceDetectorOptions({ inputSize, scoreThreshold })
     }
 
@@ -49,15 +50,8 @@ function sayReady() {
     addOutput("webcam ready...")
 }
 
-let showingID = 1;
-
-function showOption(index) {
-    console.log(`show option ${index}`)
-    showingID = index;
-}
-
 function setupWebcam() {
-    addOutput("setting up webcam...")
+    addOutput("setting up webcam...66")
     return new Promise( (resolve, reject) => {
         const navigatorAny = navigator;
         navigator.getUserMedia = navigator.getUserMedia || navigatorAny.webkitGetUserMedia || navigatorAny.mozGetUserMedia ||
@@ -87,9 +81,6 @@ function isFaceDetectionModelLoaded() {
 
 let ModelReady = false;
 
-
-var foob = true;
-
 async function updateResults() {
 
     if (!ModelReady) {
@@ -103,155 +94,75 @@ async function updateResults() {
         return setTimeout( () => updateResults() )
     }
 
-    const results = await faceapi.detectAllFaces(webcam, options).withFaceLandmarks().withFaceDescriptors();
+    const results = await faceapi.detectAllFaces(webcam, options).withFaceLandmarks();
 
     if (results[0] ) {
-
-        if ( foob == true ) {
-            console.log("boom:")
-            console.log(results);
-            foob = false;
-        }
-
         sketch.clearRect(0, 0, canvas.width, canvas.height);
-        for ( let i = 0; i < results.length; i++)
-        {
-            switch ( showingID) {
-                case 1:
-                    drawBBox(results[i]["alignedRect"]["box"], results[i]["alignedRect"]["classScore"]) 
-                    break;
-                case 2:
-                    drawPoints(results[i]["landmarks"]["positions"])
-                    break;
-                case 3:
-                    drawIndices(results[i]["landmarks"]["positions"])
-                    break;
-                case 4:
-                    drawMesh(results[i]["landmarks"]["positions"])
-                    break;
-            }
-        }
-    } else { console.log("no result obtained")}
+        var points = results[0]["landmarks"]["positions"]
+        sendpoints = {
+            nose : points[30],
+            chin : points[8],
+            left_eye : points[36],
+            right_eye : points[45],
+            left_mouth : points[48],
+            right_mouth : points[54],
+            webcam_width : webcam.width,
+            webcam_height : webcam.height
+        };
+        drawIndices(points)
+        testSocket();
+    }
     setTimeout( ()=> updateResults())
 }
 
+var sendpoints = null;
+
 function drawIndices(points) {
     sketch.fillStyle = "#FFFFFF"
-    sketch.font = "8px Sans-serif";
+    sketch.font = "10px Sans-serif";
     for ( let x = 0; x < points.length; x++ ) {
         let p = points[x];
         sketch.fillText(x, p.x, p.y)
     }
-}
-
-function drawPoints(points) {
-    sketch.fillStyle = "#00FFFF"
-    for ( let x = 0; x < points.length; x++ ) {
-        let p = points[x];
-        sketch.fillRect(p.x - 1, p.y-1, 2, 2 )
+    if ( sendpoints != null ) {
+        drawForward(sendpoints["nose"])
     }
 }
 
+var socket = io.connect('http://' + document.domain + ":" + location.port + "/eq");
 
-function drawMesh(points) {
+socket.on('connect', function() {
+    console.log('connected');
+});
 
-    sketch.strokeStyle = "#FFFFFF88"
-    sketch.lineWidth = 1;
+socket.on('system_ready', function(data) {
+    console.log("system status: " + data.status);
+});
 
-    // DRAW JAW LINE
-    var pathString = `m ${points[1].x} ${points[1].y} `
-    for ( let x = 2; x < 16; x++ ) {
-        pathString += `L ${points[x].x} ${points[x].y}`        
-    }
-    sketch.stroke(new Path2D(pathString));
-    
-    // DRAW LEFT EYE LOOP
-    pathString = `m ${points[36].x} ${points[36].y} `
-    for ( let x = 37; x < 42; x++ ) {
-        pathString += `L ${points[x].x} ${points[x].y}`        
-    }
-    pathString += "z";
-    sketch.stroke(new Path2D(pathString));
+function testSocket() {
+    console.log("to sending...")
+    console.log(sendpoints);
+    socket.emit("point_submit", { data: sendpoints })
+}
 
-    // DRAW RIGHT EYE LOOP
-    pathString = `m ${points[42].x} ${points[42].y} `
-    for ( let x = 43; x < 48; x++ ) {
-        pathString += `L ${points[x].x} ${points[x].y}`        
-    }
-    pathString += "z";
-    sketch.stroke(new Path2D(pathString));
-    
-    // DRAW EYE BROWS TOP
-    pathString = `m ${points[0].x} ${points[0].y} `
-    for ( let x = 17; x < 27; x++ ) { 
-        pathString += `L ${points[x].x} ${points[x].y}` 
-    }
-    pathString += `L ${points[16].x} ${points[16].y}` 
-    sketch.stroke(new Path2D(pathString));
-    
-    
-    // DRAW LEFT EYE HOOD
-    pathString = `m ${points[21].x} ${points[21].y} L ${points[27].x} ${points[27].y}` 
-    let downCount = 21;
-    for ( let x = 39; x > 35; x-- ) { 
-        pathString += `L ${points[x].x} ${points[x].y}` 
-        pathString += `L ${points[downCount].x} ${points[downCount].y}` 
-        downCount--
-    }
-    sketch.stroke(new Path2D(pathString));
-
-    
-    // DRAW RIGHT EYE HOOD
-    pathString = `m ${points[27].x} ${points[27].y}` 
-    downCount = 22;
-    for ( let x = 42; x < 46; x++ ) { 
-        pathString += `L ${points[x].x} ${points[x].y}` 
-        pathString += `L ${points[downCount].x} ${points[downCount].y}` 
-        downCount++;
-    }
-    sketch.stroke(new Path2D(pathString));
-    
-    // DRAW NOSE LINE
-    pathString = `m ${points[22].x} ${points[22].y} `
-    for ( let x = 27; x <= 30; x++ ) { pathString += `L ${points[x].x} ${points[x].y}` }   
-    sketch.stroke(new Path2D(pathString));
-    sketch.stroke(new Path2D(`m${points[31].x} ${points[31].y} L ${points[33].x} ${points[33].y} L ${points[35].x} ${points[35].y} `))
- 
-    // DRAW MOUTH LOOP
-    pathString = `m ${points[48].x} ${points[48].y}` 
-    for ( let x = 49; x < 68; x++ ) { 
-        pathString += `L ${points[x].x} ${points[x].y}` 
-    }
-    sketch.stroke(new Path2D(pathString));
-
-    // DRAW BLUE CONTOURS
-    sketch.strokeStyle = "#00FFFF66"
-    sketch.stroke(new Path2D(`m${points[1].x} ${points[1].y} L ${points[8].x} ${points[8].y} L ${points[15].x} ${points[15].y} z`))
-
-    sketch.stroke(new Path2D(`m${points[30].x} ${points[30].y} L ${points[39].x} ${points[39].y} L ${points[31].x} ${points[31].y} z`))
-    sketch.stroke(new Path2D(`m${points[30].x} ${points[30].y} L ${points[42].x} ${points[42].y} L ${points[35].x} ${points[35].y} z`))
-    sketch.stroke(new Path2D(`m${points[36].x} ${points[36].y} L ${points[0].x} ${points[0].y} L ${points[31].x} ${points[31].y} L ${points[48].x} ${points[48].y}`))
-    sketch.stroke(new Path2D(`m${points[45].x} ${points[45].y} L ${points[16].x} ${points[16].y} L ${points[35].x} ${points[35].y} L ${points[54].x} ${points[54].y}`))
-    sketch.stroke(new Path2D(`m${points[30].x} ${points[30].y} L ${points[33].x} ${points[33].y} L ${points[51].x} ${points[51].y}`))
+function drawForward(point_nose) {
+    sketch.beginPath();
+    sketch.moveTo(point_nose._x, point_nose._y);
+    sketch.lineTo(forwardX, forwardY);
+    sketch.strokeStyle = "red";
+    sketch.stroke();
 
 }
 
-function drawBBox(box, perc) {
-    //console.log(box);
-    sketch.clearRect(0, 0, canvas.width, canvas.height);
-    if (sketch ) {
-        sketch.strokeStyle = "#00aa0099"
-        sketch.fillStyle = "#00aa0099"
-        sketch.strokeRect(box.x, box.y, box.width, box.height)
-        sketch.fillRect(box.x, box.y + box.height, box.width, 18)
-        sketch.fillStyle = "#FFFFFF"
-        sketch.font = "11px Sans-serif";
-        sketch.fillText(`score: ${parseFloat(perc*100).toFixed(4)}`, box.x + 4, box.y + box.height + 12);
+var forwardX = 0;
+var forwardY = 0;
 
-    } else {
-        addOutput("no drawing surface available.")
-    }
-}
+socket.on('point_returned', function(dataraw) {
+    data = JSON.parse(dataraw)
+    forwardX = data.forward_x;
+    forwardY = data.forward_y;
+    txtOutput.innerText = forwardX + ", " + forwardY
+});
 
 
 document.addEventListener("DOMContentLoaded", (e)=> { setup(); })
